4c7e504318ebc439eb8df0c671fd8cc8e216e301
Jaishree2310 <jaishrees23102001@gmail.com>
2023-08-05 23:30:08 +0530

    newfile

---
+++ Day3_Q2.java
@@ -0,0 +1,44 @@
+public class Day3_Q2 {
+
+    public int getDecimalValue(ListNode head) {
+        int len = 0;
+        int sum = 0;
+        ListNode curr = head;
+        while (curr != null) {
+            len++;
+            curr = curr.next;
+        }
+        len--;
+        curr = head;
+        while (curr != null) {
+            sum += curr.val * (int) Math.pow(2, len--);
+            curr = curr.next;
+        }
+
+        return sum;
+    }
+
+    // Definition of the ListNode class
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static void main(String[] args) {
+        // Create the linked list: 1 -> 0 -> 1
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(0);
+        head.next.next = new ListNode(1);
+
+        // Create the instance of the class and call the method
+        Day3_Q2 solution = new Day3_Q2();
+        int decimalValue = solution.getDecimalValue(head);
+
+        // Print the result
+        System.out.println("Decimal Value: " + decimalValue);
+    }
+}
