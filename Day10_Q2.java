9208f28a975999cf3b65e225a8e521ecc6c24a7e
Jaishree2310 <jaishrees23102001@gmail.com>
2023-08-12 23:37:03 +0530

    newfile

---
+++ Day10_Q2.java
@@ -0,0 +1,15 @@
+//13. Roman to Integer
+
+public class Day10_Q2 {
+    public int romanToInt(String s) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+    
+        Day10_Q2 solution = new Day10_Q2();
+        String romanNumeral = "XXIV"; 
+        int intValue = solution.romanToInt(romanNumeral);
+        System.out.println("Integer value: " + intValue);
+    }
+}
