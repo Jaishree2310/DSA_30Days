3b23674da3276234959520ea97aada9d8dbdb749
Jaishree2310 <jaishrees23102001@gmail.com>
2023-08-13 23:08:14 +0530

    newfile

---
+++ Day11_Q2.java
@@ -0,0 +1,18 @@
+//11. Container With Most Water
+
+public class Day11_Q2 {
+    public static void main(String[] args) {
+        // Create an instance of Day11_Q2 and call the maxArea method here
+        Day11_Q2 solution = new Day11_Q2();
+        int[] heights = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        int result = solution.maxArea(heights);
+
+        // Print the result or perform any other operations you need
+        System.out.println("Max Area: " + result);
+    }
+
+    public int maxArea(int[] height) {
+        return 0;
+        
+    }
+}
