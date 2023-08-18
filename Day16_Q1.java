95b83779022733f5d98721b8abd831571d37f1fe
Jaishree2310 <jaishrees23102001@gmail.com>
2023-08-18 23:32:27 +0530

    newfile

---
+++ Day16_Q1.java
@@ -0,0 +1,47 @@
+//112. Path Sum
+
+public class Day16_Q1 {
+
+    public static void main(String[] args) {
+        // Create your binary tree here
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(11);
+        root.left.left.left = new TreeNode(7);
+        root.left.left.right = new TreeNode(2);
+        root.right.left = new TreeNode(13);
+        root.right.right = new TreeNode(4);
+        root.right.right.right = new TreeNode(1);
+
+        int targetSum = 22;
+
+        Day16_Q1 solution = new Day16_Q1();
+        boolean hasPath = solution.hasPathSum(root, targetSum);
+        System.out.println("Has path sum: " + hasPath);
+    }
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.left == null && root.right == null && root.val == targetSum) {
+            return true;
+        }
+
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+
+    // TreeNode class definition
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
+
